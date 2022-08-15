a815817e9a4e73051827a7d49fd8701d82db20c3
Zach Thomas <zach@Zachs-MacBook-Pro.local>
2022-08-15 12:20:16 +0200

    Completed adding tests

---
+++ test/system/questions_test.rb
@@ -0,0 +1,31 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "h1", text: "Ask your coach anything"
+  end
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "answer", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+
+  test "asking a question yields, 'Silly question, get dressed and go to work" do
+    visit ask_url
+    fill_in "answer", with: "Can I eat?"
+    click_on "Ask"
+
+    assert_text "Silly question, get dressed and go to work!"
+  end
+  
+  
+  # test "visiting the index" do
+  #   visit questions_url
+  #
+  #   assert_selector "h1", text: "Questions"
+  # end
+end
